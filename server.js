61dab5f4d904fc05ff4f31312ff6e5c56ae56533
Nathan Thomas <nwthomas@me.com>
2019-02-13 13:05:57 -0800

    Implement .get() and .getByID() functionality for users

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
+const usersRouter = require("./users/users-router.js");
+
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 server.use(morgan("dev"));
+
+server.use("/api/users", usersRouter);
 
 server.get("/", async (req, res, next) => {
   res.send(`
--- users/users-router.js
+++ users/users-router.js
@@ -1,17 +1,35 @@
 const express = require("express");
 
-const Users = require("../data/seeds/02-users.js");
+const Users = require("../data/helpers/userDb.js");
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
+  console.log(req);
   try {
-    const users = await Users.find(req.query);
+    const users = await Users.get(req.query);
     res.status(200).json(users);
   } catch (error) {
     console.log(error);
     res.status(500).json({
       message: "Error retrieving the users."
+    });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  console.log(req);
+  try {
+    const user = await Users.getById(req.params.id);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: "User not found." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error retrieving the hub."
     });
   }
 });
