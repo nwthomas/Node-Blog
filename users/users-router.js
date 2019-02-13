abd8b987478cb59e9be6257b2d6093aeaf8474a8
Nathan Thomas <nwthomas@me.com>
2019-02-13 13:50:15 -0800

    Make .toUpperCase() middleware for users name in post/put functions

--- users/users-router.js
+++ users/users-router.js
@@ -4,8 +4,12 @@ const Users = require("../data/helpers/userDb.js");
 
 const router = express.Router();
 
+function uppercase(req, res, next) {
+  req.body.name = req.body.name.toUpperCase();
+  next();
+}
+
 router.get("/", async (req, res) => {
-  console.log(req);
   try {
     const users = await Users.get(req.query);
     res.status(200).json(users);
@@ -18,7 +22,6 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-  console.log(req);
   try {
     const user = await Users.getById(req.params.id);
     if (user) {
@@ -34,8 +37,10 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {
-  console.log(req);
+router.post("/", uppercase, async (req, res) => {
+  if (!req.body.name) {
+    res.status();
+  }
   try {
     const user = await Users.insert(req.body);
     res.status(201).json(user);
@@ -43,6 +48,36 @@ router.post("/", async (req, res) => {
     console.log(error);
     res.status(500).json({
       message: "Error adding the user."
+    });
+  }
+});
+
+router.put("/:id", uppercase, async (req, res) => {
+  try {
+    const user = await Users.update(req.params.id, req.body);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: "The user could not be found." });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Error updating the user."
+    });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const user = await User.remove(req.params.id);
+    if (user > 0) {
+      res.status(200).json({ message: "The user has been deleted." });
+    } else {
+      res.status(404).json({ message: "The user could not be found." });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Error deleting the user."
     });
   }
 });
