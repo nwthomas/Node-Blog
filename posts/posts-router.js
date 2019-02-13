334bc3da6b2f86c83c59d2ae3e21a813b9b142dd
Nathan Thomas <nwthomas@me.com>
2019-02-13 15:41:49 -0800

    Make .delete() request for posts work

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -65,4 +65,21 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Posts.remove(req.params.id);
+    if (post) {
+      res
+        .status(200)
+        .json({ message: "The post has been deleted.", numPostsDeleted: post });
+    } else {
+      res.status(404).json({ message: "The post could not be found." });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Error deleting the post."
+    });
+  }
+});
+
 module.exports = router;
