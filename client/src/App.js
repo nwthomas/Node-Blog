9542787d5794cfa27360eaa40bcc59f806d641c7
Nathan Thomas <nwthomas@me.com>
2019-02-13 16:13:05 -0800

    Retrieve user list in client and display in list

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { UserListContainer } from "./components/UserListContainer";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    users: [],
+    posts: []
+  };
+  componentDidMount() {
+    axios
+      .get("http://localhost:4000/api/users")
+      .then(res => {
+        this.setState({
+          ...this.state,
+          users: res.data,
+          message: res.statusText
+        });
+      })
+      .catch(err =>
+        this.setState({
+          ...this.state,
+          message: err.statusText
+        })
+      );
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <UserListContainer posts={this.state.posts} users={this.state.users} />
       </div>
     );
   }
